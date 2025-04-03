e378eaa772f79d2a208f78868bc4ddcded3c237b
SRshohan <srshohan02@gmail.com>
2025-04-02 23:17:14 -0400

    Added liveness

--- facial_id/app.py
+++ facial_id/app.py
@@ -15,27 +15,7 @@ def biometric(img1_p, img2_p):
     print(result["verified"])
     return result["verified"]
 
-if __name__ == "__main__":
-    # question = input("Do you want to register(press 1) or login(press 2)? ")
-    # if question == "2":
-    #     name = input("Please enter your name: ")
-    #     # Process 1: Run MediaPipe
-    #     p1 = Process(target=draw_landmarks_on_camera, kwargs={"output_path": f"verification/{name}.jpg", "task":"verification"})
-    #     p1.start()
-    #     p1.join()  # Wait for the process to complete
 
-    #     # Process 2: Run DeepFace
-    #     p2 = Process(target=biometric, args=(f"registration/{name}.jpg", f"verification/{name}.jpg"))
-    #     p2.start()
-    #     p2.join()
-    # else:
-    #     name = input("Please enter your name: ")
-    #     p3 = Process(target=draw_landmarks_on_camera, kwargs={"output_path": f"registration/{name}.jpg", "task":"Registration scan"})
-    #     p3.start()
-    #     p3.join()
-    # if os.path.exists("captured.jpg"):
-    #     print(__extract_faces_and_embeddings("captured_image.jpg"))
-    # else:
-    #     print("It doesn't exist")
+if __name__ == "__main__":
     res = facial_embeddings("00234_registered_face.jpg")
     print(len(res))
